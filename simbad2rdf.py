a4193d06855e43d2da0f9c32d35661a5f7c7751d
Rahul Dave <rahuldave@gmail.com>
2011-03-10 12:23:46 -0500

    merged more stuff from Doug

--- simbad2rdf.py
+++ simbad2rdf.py
@@ -8,8 +8,22 @@ from rdflib import ConjunctiveGraph
 import uuid
 from lxml import etree as ElementTree
 import HTMLParser
-
 import os.path
+
+
+"""
+{
+    '1999ApJ...522..718Q': [
+        {'mtype': '~,~,~', 'otype': 'LINER ~', 'refcode': '1999ApJ...522..718Q', 'ra': '184.84673421', '_raw': 'NGC  4261|184.84673421|+05.82491522|LINER ~|~,~,~|E...,D,~', 'dec': '+05.82491522', 'id': 'NGC  4261'}, 
+        {'mtype': '~,~,~', 'otype': 'Seyfert_2 ~', 'refcode': '1999ApJ...522..718Q', 'ra': '186.26559721', '_raw': 'M  84|186.26559721|+12.88698314|Seyfert_2 ~|~,~,~|E...,D,~', 'dec': '+12.88698314', 'id': 'M  84'}, 
+        {'mtype': '~,~,~', 'otype': 'GinGroup ~', 'refcode': '1999ApJ...522..718Q', 'ra': '193.092133', '_raw': 'NGC  4753|193.092133|-01.199689|GinGroup ~|~,~,~|I...,D,~', 'dec': '-01.199689', 'id': 'NGC  4753'}, 
+        {'mtype': '~,~,~', 'otype': 'Seyfert_2 ~', 'refcode': '1999ApJ...522..718Q', 'ra': '201.36506279', '_raw': 'NGC  5128|201.36506279|-43.01911258|Seyfert_2 ~|~,~,~|E,D,~', 'dec': '-43.01911258', 'id': 'NGC  5128'}
+    ]
+}
+"""
+
+
+
 filetoread=sys.argv[1]
 fd=open(filetoread)
 stuff=fd.read()
